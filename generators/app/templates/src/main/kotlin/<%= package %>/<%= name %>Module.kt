0d3b1f6f0dc3a9a0e3234583259d6073fe7f19d9
mega-mac-slice <megamacslice@protonmail.com>
2018-12-19 20:44:52 -0600

    Add config DI.
    - Allows configuration to be injectable.
    
    Issues: NA

--- generators/app/templates/src/main/kotlin/<%= package %>/<%= name %>Module.kt
+++ generators/app/templates/src/main/kotlin/<%= package %>/<%= name %>Module.kt
@@ -1,8 +1,16 @@
 package <%= package %>
 
 import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class <%= name %>Module(
         private val configuration: <%= name %>Configuration
-)
+) {
+    @Provides
+    @Singleton
+    fun getConfiguration(): <%= name %>Configuration {
+        return configuration
+    }
+}
